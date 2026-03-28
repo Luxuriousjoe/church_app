1b0606c34a245142884d5e77e84a59ceec23935c
Luxuriousjoe <156549315+Luxuriousjoe@users.noreply.github.com>
2026-03-28 12:21:36 -0400

    Revert "upload"
    This reverts commit a396053a9f3452f514d59e58011d6cab4b34aa20.

--- flutter/android/app/src/main/kotlin/com/example/church_media_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.church_media_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter/lib/widgets/shimmer_loader.dart
+++
@@ -1,2 +0,0 @@
-// Re-export from media_tile.dart for clean imports
-export 'media_tile.dart' show ShimmerLoader, ShimmerListTile;
